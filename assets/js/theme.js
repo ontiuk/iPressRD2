10b59d2862408af2bbab957189e576b6a7f1c974
Stephen Betley <ontiuk@hotmail.com>
2022-03-14 12:34:40 +0000

    Update theme.js
    Theme JS

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -6,6 +6,28 @@
 	var body	= $('body'),
 		_window = $(window);
 	
+	// Clunky but useable mobile detection
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android|webOS/i);
+		},
+		BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+		iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+		Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+		Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+		any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+
 	// On window load functions
 	_window.on('load', function(){});
 
